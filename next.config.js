3cced2646688c0a5dce9e48492833381ab00b785
ocizek <ocizek@icloud.com>
2025-07-14 11:11:44 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  // NESMÍ tu být output: "export"!
+}
 
 module.exports = nextConfig
