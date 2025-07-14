582f4a544cd5154f3fd6c716169f8a16eb920f52
ocizek <ocizek@icloud.com>
2025-07-14 10:54:26 +0200

    Update [id].tsx

--- pages/trip/[id].tsx
+++ pages/trip/[id].tsx
@@ -140,3 +140,16 @@ export default function TripDetailPage() {
     </motion.div>
   );
 }
+// Pro Next.js 13/14 v pages/
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: true
+  };
+}
+
+export async function getStaticProps() {
+  return {
+    props: {}
+  };
+}
